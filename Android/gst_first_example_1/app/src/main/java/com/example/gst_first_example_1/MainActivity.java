9a243263ef23d02ca3eca472a531e5618f697702
dadm8473 <dadm8473@gmail.com>
2019-11-07 20:57:41 +0900

    Update

---
+++ Android/gst_first_example_1/app/src/main/java/com/example/gst_first_example_1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.gst_first_example_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.freedesktop.gstreamer.GStreamer;
+
+public class MainActivity extends AppCompatActivity {
+    private native String nativeGetGStreamerInfo();
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        try {
+            GStreamer.init(this);
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView)findViewById(R.id.textview_info);
+        tv.setText("Welcome to " + nativeGetGStreamerInfo() + " !");
+    }
+    static {
+        System.loadLibrary("gstreamer_android");
+        System.loadLibrary("tutorial-1");
+    }
+}
---
+++ Android/정리/HelloJNI/app/src/main/cpp/Application.mk
@@ -0,0 +1,2 @@
+APP_STL := c++_shared
+APP_ABI := armeabi armeabi-v7a x86
